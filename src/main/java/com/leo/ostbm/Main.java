89f5bf6e31bd9c02f475050c9fe619e99980ee60
Leo40Git <kfir40mailminer@gmail.com>
2017-10-08 16:46:04 +0300

    apparently i never released 1.7 so

--- src/main/java/com/leo/ostbm/Main.java
+++ src/main/java/com/leo/ostbm/Main.java
@@ -49,7 +49,7 @@ public class Main {
 
 	public static final Logger LOGGER = LogManager.getLogger("OSTBM");
 
-	public static final Version VERSION = new Version("1.7");
+	public static final Version VERSION = new Version("1.6");
 	public static final String UPDATE_CHECK_SITE = "https://raw.githubusercontent.com/Leo40Git/OneShot-Textbox-Maker/master/.version";
 	public static final String DOWNLOAD_SITE = "https://github.com/Leo40Git/OneShot-Textbox-Maker/releases/latest/";
 	public static final String ISSUES_SITE = "https://github.com/Leo40Git/OneShot-Textbox-Maker/issues";
