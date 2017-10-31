c6ddb6dccb780198dad4e3f2b8c36e2f01e89d74
Leo40Git <kfir40mailminer@gmail.com>
2017-10-31 16:07:30 +0200

    Make sure map can't be modified via entry.setValue

--- src/main/java/com/leo/ostbm/util/UnmodifiableMapWrapper.java
+++ src/main/java/com/leo/ostbm/util/UnmodifiableMapWrapper.java
@@ -1,13 +1,14 @@
 package com.leo.ostbm.util;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class UnmodifiableMapWrapper<K, V> implements Map<K, V> {
-	
+
 	private Map<K, V> wrapped;
-	
+
 	public UnmodifiableMapWrapper(Map<K, V> wrapped) {
 		this.wrapped = wrapped;
 	}
@@ -65,9 +66,39 @@ public class UnmodifiableMapWrapper<K, V> implements Map<K, V> {
 		return new UnmodifiableCollectionWrapper<>(wrapped.values());
 	}
 
+	@SuppressWarnings("hiding")
+	public class UnmodifiableEntryWrapper<K, V> implements Entry<K, V> {
+
+		private Entry<K, V> wrapped;
+
+		public UnmodifiableEntryWrapper(Entry<K, V> wrapped) {
+			this.wrapped = wrapped;
+		}
+
+		@Override
+		public K getKey() {
+			return wrapped.getKey();
+		}
+
+		@Override
+		public V getValue() {
+			return wrapped.getValue();
+		}
+
+		@Override
+		public V setValue(V value) {
+			return null;
+		}
+
+	}
+
 	@Override
 	public Set<Entry<K, V>> entrySet() {
-		return new UnmodifiableSetWrapper<>(wrapped.entrySet());
+		Set<Entry<K, V>> entrySet = wrapped.entrySet();
+		Set<Entry<K, V>> wEntrySet = new HashSet<>();
+		for (Entry<K, V> entry : entrySet)
+			wEntrySet.add(new UnmodifiableEntryWrapper<>(entry));
+		return wEntrySet;
 	}
 
 }
